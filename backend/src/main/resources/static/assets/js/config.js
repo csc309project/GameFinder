de7968689cea5741066818acbf8a398a23df4d82
ethana1234 <ethana1234@gmail.com>
2019-05-12 14:57:20 -0700

    Removed useless backend folder

--- backend/src/main/resources/static/assets/js/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const dotenv = require('dotenv');
-dotenv.config();
-module.exports = {
-    steamKey: process.env.STEAM_API_KEY,
-    steamBaseURL: process.env.STEAM_BASE_URL,
-    exampleProfileID: process.env.ETHANS_STEAM_PROFILE_ID
-};
