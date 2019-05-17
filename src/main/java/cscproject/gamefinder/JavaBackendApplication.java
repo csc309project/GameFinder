4dccf42f382696584b0eb6fee411c1196ecccc08
ethana1234 <ethana1234@gmail.com>
2019-05-17 12:32:02 -0700

    Trying to integrate jsp

--- src/main/java/cscproject/gamefinder/JavaBackendApplication.java
+++ src/main/java/cscproject/gamefinder/JavaBackendApplication.java
@@ -2,9 +2,16 @@ package cscproject.gamefinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaBackendApplication {
+public class JavaBackendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JavaBackendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JavaBackendApplication.class, args);
