9767676366f34a32a713854ebc61a28514cadb4d
Justin To <hellojkt@hotmail.com>
2019-05-27 14:18:45 -0700

    made a User class

---
+++ src/main/java/cscproject/gamefinder/User/User.java
@@ -0,0 +1,41 @@
+package cscproject.gamefinder.User;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long user_id;
+
+    private String username;
+    private String password;
+
+    public User(String fname, String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {}
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String passWord) {
+        this.password = passWord;
+    }
+
+}
