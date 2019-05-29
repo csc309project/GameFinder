280426f52996a815c1c9228ae53e04cc4d6e7d83
Justin To <hellojkt@hotmail.com>
2019-05-29 13:49:13 -0700

    parser is almost good to go

--- src/main/java/cscproject/gamefinder/game/Parse.java
+++ src/main/java/cscproject/gamefinder/game/Parse.java
@@ -1,8 +1,11 @@
 package cscproject.gamefinder.game;
 
 import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.io.File;
+import java.util.Iterator;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -10,13 +13,25 @@ import java.io.IOException;
 
 public class Parse {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         JSONParser parser = new JSONParser();
 
         try {
-            Object obj = parser.parse(new FileReader("games.json"));
+            String fname = new File(".").getCanonicalPath();
+            File file = new File(fname, "/src/main/resources/json/games.json");
+            Object obj = parser.parse(new FileReader(file));
             JSONObject jsonObject = (JSONObject) obj;
-            
+
+            JSONArray arr = (JSONArray) jsonObject.get("gamesList");
+            Iterator iterator = arr.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+            //String description = (String) jsonObject.get("description");
+            //String steamURL = (String) jsonObject.get("steamURl");
+            //String reviews = (String) jsonObject.get("reviews");
+            //String price = (String) jsonObject.get("price");
 
         }
         catch (FileNotFoundException err) {err.printStackTrace();}
