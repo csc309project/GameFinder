eed1fa254003a0d884b50ced81b33d6af7fd0ba5
Justin To <hellojkt@hotmail.com>
2019-05-29 14:44:17 -0700

    parser is done

--- src/main/java/cscproject/gamefinder/game/Parse.java
+++ src/main/java/cscproject/gamefinder/game/Parse.java
@@ -5,6 +5,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Iterator;
 
 import java.io.FileNotFoundException;
@@ -13,8 +15,9 @@ import java.io.IOException;
 
 public class Parse {
 
-    public static void main(String[] args) throws Exception {
+    public List<Game> createGameList(String[] args) throws Exception {
         JSONParser parser = new JSONParser();
+        ArrayList<Game> game_arr = new ArrayList<Game>();
 
         try {
             String fname = new File(".").getCanonicalPath();
@@ -22,22 +25,24 @@ public class Parse {
             Object obj = parser.parse(new FileReader(file));
             JSONObject jsonObject = (JSONObject) obj;
 
-            JSONArray arr = (JSONArray) jsonObject.get("gamesList");
-            Iterator iterator = arr.iterator();
+            JSONArray JSONarr = (JSONArray) jsonObject.get("gamesList");
+            Iterator iterator = JSONarr.iterator();
+
             while (iterator.hasNext()) {
-                System.out.println(iterator.next());
+                JSONObject arr_obj = (JSONObject) iterator.next();
+                Game temp = new Game((String) arr_obj.get("name"), (String) arr_obj.get("description"),
+                                     (String) arr_obj.get("steamURl"), (String) arr_obj.get("reviews"),
+                                     (double) arr_obj.get("price"));
+                game_arr.add(temp);
             }
-
-            //String description = (String) jsonObject.get("description");
-            //String steamURL = (String) jsonObject.get("steamURl");
-            //String reviews = (String) jsonObject.get("reviews");
-            //String price = (String) jsonObject.get("price");
-
+            return game_arr;
         }
         catch (FileNotFoundException err) {err.printStackTrace();}
-        catch (IOException err) {err.printStackTrace();}
-        catch (ParseException err) {err.printStackTrace();}
-        catch (Exception err) {err.printStackTrace();}
+        //catch (IOException err) {err.printStackTrace();}
+        //catch (ParseException err) {err.printStackTrace();}
+        //catch (Exception err) {err.printStackTrace();}
+
+        return game_arr;
         
     }
 
