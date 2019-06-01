4b7198bbcc0f19e2d4dfd9433b81e60f02cc914a
Justin To <hellojkt@hotmail.com>
2019-06-01 11:30:18 -0700

    code smells

--- src/main/java/cscproject/gamefinder/game/Parse.java
+++ src/main/java/cscproject/gamefinder/game/Parse.java
@@ -9,19 +9,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Parse {
 
-    public static List<Game> createGameList() throws Exception {
+    public static List<Game> createGameList() throws ParseException {
         JSONParser parser = new JSONParser();
-        ArrayList<Game> game_arr = new ArrayList<Game>();
+        ArrayList<Game> gameArr = new ArrayList<>();
 
         try {
             String fname = new File(".").getCanonicalPath();
-            File file = new File(fname, "/src/main/resources/json/games.json");
+            String  loc = "/src/main/resources/json/games.json";
+            File file = new File(fname, loc);
             Object obj = parser.parse(new FileReader(file));
             JSONObject jsonObject = (JSONObject) obj;
 
@@ -29,20 +29,19 @@ public class Parse {
             Iterator iterator = JSONarr.iterator();
 
             while (iterator.hasNext()) {
-                JSONObject arr_obj = (JSONObject) iterator.next();
-                Game temp = new Game((String) arr_obj.get("name"), (String) arr_obj.get("description"),
-                                     (String) arr_obj.get("steamURl"), (String) arr_obj.get("reviews"),
-                                     (double) arr_obj.get("price"));
-                game_arr.add(temp);
+                JSONObject arrObj = (JSONObject) iterator.next();
+                Game temp = new Game((String) arrObj.get("name"), (String) arrObj.get("description"),
+                                     (String) arrObj.get("steamURl"), (String) arrObj.get("reviews"),
+                                     (double) arrObj.get("price"));
+                gameArr.add(temp);
             }
-            return game_arr;
-        }
-        catch (FileNotFoundException err) {err.printStackTrace();}
-        //catch (IOException err) {err.printStackTrace();}
-        //catch (ParseException err) {err.printStackTrace();}
-        //catch (Exception err) {err.printStackTrace();}
 
-        return game_arr;
+            return gameArr;
+        }
+
+        catch (IOException | ParseException err) {err.printStackTrace();}
+
+        return gameArr;
         
     }
 
