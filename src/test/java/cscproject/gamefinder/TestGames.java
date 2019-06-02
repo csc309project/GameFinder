ee6c584f330f1d2ee56310a984425d94e6fbd270
Justin To <hellojkt@hotmail.com>
2019-06-01 19:39:04 -0700

    put and delete return void

--- src/test/java/cscproject/gamefinder/TestGames.java
+++ src/test/java/cscproject/gamefinder/TestGames.java
@@ -1,5 +1,6 @@
 package cscproject.gamefinder;
 
+import cscproject.gamefinder.game.Game;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -26,30 +28,26 @@ public class TestGames {
 	public void testGetGameListSuccess() throws URISyntaxException
 	{
 		RestTemplate restTemplate = new RestTemplate();
-
 		final String baseUrl = "http://localhost:" + randomServerPort + "/api/game";
 		URI uri = new URI(baseUrl);
-
 		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
 
 		assertEquals(200, result.getStatusCodeValue());
 	}
 
 	@Test
-	public void testGameResponseBody() throws URISyntaxException {
+	public void testGameGet() throws URISyntaxException {
 
 		RestTemplate restTemplate = new RestTemplate();
-
 		final String baseUrl = "http://localhost:" + randomServerPort + "/api/game/DOTA%202";
 		URI uri = new URI(baseUrl);
-
 		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
 
 		assertEquals(200, result.getStatusCodeValue());
 	}
 
 	@Test
-	public void testGameFindFail() throws URISyntaxException {
+	public void testGameGetFail() throws URISyntaxException {
 		RestTemplate restTemplate = new RestTemplate();
 
 		final String baseUrl = "http://localhost:" + randomServerPort + "/api/game/DoesNotExist";
@@ -64,6 +62,35 @@ public class TestGames {
 			assertEquals(true, err.getResponseBodyAsString().contains("Could not be found"));
 		}
 	}
+
+	@Test
+	public void testGamePost() throws URISyntaxException{
+		RestTemplate restTemplate = new RestTemplate();
+
+		final String baseUrl = "http://localhost:" + randomServerPort + "/api/game";
+		URI uri = new URI(baseUrl);
+		Game dummy = new Game("name", "description", "steam_url", "reviews", 0.0);
+		ResponseEntity<String> result = restTemplate.postForEntity(uri, dummy, String.class);
+
+		assertEquals(200, result.getStatusCodeValue());
+	}
+
+/*	@Test
+	public void testGamePut() throws URISyntaxException{
+		RestTemplate restTemplate = new RestTemplate();
+
+		final String baseUrl = "http://localhost:" + randomServerPort + "/api/game";
+		URI uri = new URI(baseUrl);
+
+		HttpHeaders headers = new HttpHeaders();
+
+		Game dummy = new Game("name", "description", "steam_url", "reviews", 0.0);
+
+		ResponseEntity<String> result = restTemplate.put(uri, dummy);
+		ResponseEntity<Object> response = restTemplate.exchange(uri, HttpMethod.PUT, requestEntity, null, id);
+
+		assertEquals(200, result.getStatusCodeValue());
+	}*/
 
 
 }
---
+++ src/test/java/cscproject/gamefinder/TestUser.java
@@ -0,0 +1,33 @@
+package cscproject.gamefinder;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static junit.framework.TestCase.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestUser {
+
+    @LocalServerPort
+    int randomServerPort;
+
+    @Test
+    public void testGetUser() throws URISyntaxException {
+
+        RestTemplate restTemplate = new RestTemplate();
+        final String baseUrl = "http://localhost:" + randomServerPort + "/api/user";
+        URI uri = new URI(baseUrl);
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+    }
+}
