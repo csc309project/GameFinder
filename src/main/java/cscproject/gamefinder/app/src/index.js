c4bd3ef61adf04d0d43b44ffecdaac299ac6f36c
Nick Ryan <nick.ryan002@gmail.com>
2019-06-04 21:27:01 -0700

    Added landingPage.js and changed index.html,index.js

--- src/main/java/cscproject/gamefinder/app/src/index.js
+++ src/main/java/cscproject/gamefinder/app/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LikeButton from './like_button';
+import LandingPage from './landingPage';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 /* jshint ignore:start */
-ReactDOM.render(<App/>, document.getElementById('root'));
+//ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<LikeButton/>, document.getElementById('like_button_container'));
+ReactDOM.render(<LandingPage/>, document.getElementById('landingPage'));
 /* jshint ignore:end */
 
 // If you want your app to work offline and load faster, you can change
